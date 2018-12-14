dba6dc4906a0d1d0b8574936d9a4e7d303ac0dec
King, William Brandon <wking6@gatech.edu>
2018-12-14 07:06:05 -0500

    Import clean up issue #25

--- ui/src/app/components/announcements/announcements.component.ts
+++ ui/src/app/components/announcements/announcements.component.ts
@@ -1,9 +1,5 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MessageType} from '../../enums/message-type.enum';
-import {SidebarService} from '../../services/sidebar/sidebar.service';
-import {UserService} from '../../services/user/user.service';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-announcements',
