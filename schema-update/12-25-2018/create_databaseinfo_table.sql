b21c9d86b6eda8c87dca76361b205a50c89996fb
King, William Brandon <wking6@gatech.edu>
2018-12-25 00:59:39 -0500

    Check in SchemaUpdater tool

---
+++ schema-update/12-25-2018/create_databaseinfo_table.sql
@@ -0,0 +1,17 @@
+-- Table: public."DatabaseInfo"
+
+-- DROP TABLE public."DatabaseInfo";
+
+CREATE TABLE public."DatabaseInfo"
+(
+    dbversion integer NOT NULL,
+    uiversion double precision NOT NULL,
+    apiversion double precision NOT NULL
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."DatabaseInfo"
+    OWNER to postgres;
