956a84f20fb05aa1754dffc90292ac87c1e28e89
King, William Brandon <wking6@gatech.edu>
2019-01-04 11:58:03 -0500

    Remove oahu projects

--- oahuprojects/projects/oahu-components/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of oahu-components
- */
-
-export * from './lib/oahu-components.service';
-export * from './lib/oahu-components.component';
-export * from './lib/oahu-components.module';
